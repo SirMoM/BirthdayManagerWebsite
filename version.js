b1006b1c586ced944d702c4aa2e9621a79935685
Noah Ruben <noah.ruben@online.de>
2019-02-24 16:08:33 +0100

    v0.1.1

--- version.js
+++ version.js
@@ -1,6 +1,10 @@
 
 let version = 'Version: 0.0.1-beta';
+<<<<<<< HEAD
+let time = 'Buildtime:  24-02-2019 16:07:01';
+=======
 let time = 'Buildtime:  23-02-2019 16:29:38';
+>>>>>>> branch 'master' of https://github.com/SirMoM/BirthdayManagerWebsite
 let tab = " - "
 
 var buildInfo = document.getElementById("buildInfo");
@@ -10,6 +14,10 @@ var versionTxt = document.createTextNode(version + tab + time);
 buildInfo.appendChild(versionTxt);
 /*
 Version: 0.0.1-beta
+<<<<<<< HEAD
+Buildtime: 24-02-2019 16:07:02
+=======
 Buildtime: 23-02-2019 16:29:38
+>>>>>>> branch 'master' of https://github.com/SirMoM/BirthdayManagerWebsite
 Application-name: Birthday Manager
 */
