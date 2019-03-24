e17d4a2f14d9281d8328113fa5a5ac2293902f24
Noah Ruben <noah.ruben@online.de>
2019-03-24 12:00:20 +0100

    v0.2.0

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 
 let version = 'Version: 0.1.x-beta';
-let time = 'Buildtime:  19-03-2019 20:52:37';
+let time = 'Buildtime:  24-03-2019 11:59:07';
 let tab = " - "
 
 var buildInfo = document.getElementById("buildInfo");
@@ -10,6 +10,6 @@ var versionTxt = document.createTextNode(version + tab + time);
 buildInfo.appendChild(versionTxt);
 /*
 Version: 0.1.x-beta
-Buildtime: 19-03-2019 20:52:37
+Buildtime: 24-03-2019 11:59:07
 Application-name: Birthday Manager
 */
