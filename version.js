126d4765299abeac236529d156614d6ce8b30b93
Noah Ruben <noah.ruben@online.de>
2019-04-29 22:03:20 +0200

    v0.2.5
    - background Tasks work now
    - more preferences are possible
    - icon
    - ical

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 
 let version = 'Version: 0.1.x-beta';
-let time = 'Buildtime:  24-03-2019 11:59:07';
+let time = 'Buildtime:  29-04-2019 22:01:44';
 let tab = " - "
 
 var buildInfo = document.getElementById("buildInfo");
@@ -10,6 +10,6 @@ var versionTxt = document.createTextNode(version + tab + time);
 buildInfo.appendChild(versionTxt);
 /*
 Version: 0.1.x-beta
-Buildtime: 24-03-2019 11:59:07
+Buildtime: 29-04-2019 22:01:44
 Application-name: Birthday Manager
 */
