aa4457cb7d6f08fe7b2b139789545f956ecbcdd0
Noah Ruben <noah.ruben@online.de>
2019-05-13 20:04:50 +0200

    v0.2.5

--- version.js
+++ version.js
@@ -1,15 +1,15 @@
 
 let version = 'Version: 0.2.x-beta';
-let time = 'Buildtime:  29-04-2019 22:06:27';
+let time = 'Buildtime:  13-05-2019 20:04:22';
 let tab = " - "
 
 var buildInfo = document.getElementById("buildInfo");
 
-var versionTxt = document.createTextNode(version + tab + time + tab + tab + tab + "THIS VERSION IS NOT COMPATIBLE WITH LATER VERSIONS");
+var versionTxt = document.createTextNode(version + tab + time);
 
 buildInfo.appendChild(versionTxt);
 /*
 Version: 0.2.x-beta
-Buildtime: 29-04-2019 22:06:27
+Buildtime: 13-05-2019 20:04:22
 Application-name: Birthday Manager
 */
