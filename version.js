04413d8829300496a44f564dd1293e6cd436cc7f
SirMoM <pirat99@online.de>
2020-01-16 11:01:08 +0100

    v0.3.8

--- version.js
+++ version.js
@@ -1,6 +1,6 @@
 
 let version = 'Version: 0.3.x-beta';
-let time = 'Buildtime:  09-01-2020 15:41:28';
+let time = 'Buildtime:  16-01-2020 11:00:23';
 let tab = " - "
 
 var buildInfo = document.getElementById("buildInfo");
@@ -10,6 +10,6 @@ var versionTxt = document.createTextNode(version + tab + time);
 buildInfo.appendChild(versionTxt);
 /*
 Version: 0.3.x-beta
-Buildtime: 09-01-2020 15:41:28
+Buildtime: 16-01-2020 11:00:23
 Application-name: Birthday Manager
 */
